69f8c6f9e47f27ac05c51e57d146f293866f25c1
ekkang2 <rkddmsrud27@gmail.com>
2024-10-16 14:04:08 +0900

    add admin html/css

--- src/main/java/com/lotte4/controller/pagecontroller/admin/member/AdminMemberController.java
+++ src/main/java/com/lotte4/controller/pagecontroller/admin/member/AdminMemberController.java
@@ -1,18 +1,18 @@
-package com.lotte4.controller.pagecontroller.admin.user;
+package com.lotte4.controller.pagecontroller.admin.member;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class memberComtroller {
+public class MemberController {
 
     @GetMapping("/admin/member/list")
     public String Adminmemberlist(){
-        return "admin/member_list";
+        return "/admin/member/list";
     }
 
     @GetMapping("/admin/member/point")
     public String Adminmemberpoint(){
-        return "admin/member_point";
+        return "/admin/member/point";
     }
 }
